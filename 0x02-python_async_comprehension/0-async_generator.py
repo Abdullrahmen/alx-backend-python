a882396271199032c93ae3a8800d7626a9c8bf47
Abdullrahmen <abdullrahmen2003@gmail.com>
2024-08-31 18:46:09 +0000

    Add and fix some tasks

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
-'''Task 0's module.
-'''
+"""coroutine called async_generator"""
 import asyncio
-import random
+from random import uniform
 from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    '''Generates a sequence of 10 numbers.
-    '''
-    for _ in range(10):
+    '''Generates a sequence of 10 numbers.'''
+    for i in range(10):
         await asyncio.sleep(1)
-        yield random.random() * 10
+        yield uniform(0, 10)
